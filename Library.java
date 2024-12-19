e0aedfeb23d5f5d6ccf5b1129619a4357713045b
Kelvin Yeboah <onlykelvin06@gmail.com>
2024-12-19 05:00:23 -0800

    Update Library.java

--- Library.java
+++ Library.java
@@ -1,51 +1,29 @@
 
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class Library {
-    private List<Book> books;
-    private List<Patron> patrons;
+    private final ArrayList<Book> books;
 
     public Library() {
-        this.books = new ArrayList<>();
-        this.patrons = new ArrayList<>();
+        books = new ArrayList<>();
     }
 
     public void addBook(Book book) {
         books.add(book);
     }
 
-    public void registerPatron(Patron patron) {
-        patrons.add(patron);
+    public boolean removeBook(String title) {
+        return books.removeIf(book -> book.title().equals(title));
     }
 
-    public boolean checkoutBook(String title, Patron patron) {
-        for (Book book : books) {
-            if (book.getTitle().equals(title) && book.getAvailableCopies() > 0) {
-                book.setAvailableCopies(book.getAvailableCopies() - 1);
-                patron.borrowBook(book);
-                return true;
+    public void listBooks() {
+        if (books.isEmpty()) {
+            System.out.println("Please there are currently no books available.");
+        } else {
+            for (Book book : books) {
+                System.out.println(book.title());
             }
         }
-        return false; // Book not available
-    }
-
-    public void returnBook(String title, Patron patron) {
-        for (Book book : books) {
-            if (book.getTitle().equals(title)) {
-                book.setAvailableCopies(book.getAvailableCopies() + 1);
-                patron.returnBook(book);
-                break;
-            }
-        }
-    }
-
-    public List<Book> getBooks() {
-        return books; // Return the list of books
-    }
-
-    public List<Patron> getPatrons() {
-        return patrons; // Return the list of patrons
     }
 }
